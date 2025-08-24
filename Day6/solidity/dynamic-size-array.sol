9a69ea26934174815a57f5bd1fdaa2ef4508ab81
harshita parashar <harshitaparashar028@gmail.com>
2025-08-24 20:23:58 +0530

    Add files via upload

---
+++ Day6/solidity/dynamic-size-array.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract array {
+    uint[] public arr;
+
+    function pushElement( uint item) public
+{ arr.push(item); }
+
+ function length() public view returns (uint){
+   return arr.length;
+} 
+function popElement () public {
+  arr.pop();
+
+}
+}
+    
+
+
+
